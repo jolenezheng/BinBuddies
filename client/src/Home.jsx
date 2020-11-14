bc4b521a922e09690b4ba951ad620936b6e7db0e
Anusha Dey <aunosua00@gmail.com>
2020-11-14 18:54:59 -0500

    add edmonton api and final page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {Button, Modal} from '@material-ui/core/';
 import {Link} from "react-router-dom";
 import './App.css';
+import history from './history';
 
 function Home(props) {
   //closed by default
@@ -65,7 +66,7 @@ function Home(props) {
         onClose={handleCloseChild}
       >
         <div className="testModal">
-          <QuestionModal detectedObject={detectedObject} parentCallback={handleCloseChild}/>
+          <FetchModal detectedObject={detectedObject} parentCallback={handleCloseChild}/>
         </div>
       </Modal>
     </div>
@@ -113,11 +114,66 @@ function ManualModalParent() {
   )
 }
 
+function FetchModal(props) {
+  let {detectedObject, parentCallback} = props;
+  let [init, setInit] = useState(false);
+  let [isPredefined, setIsPredefined] = useState(false);
+  let [data, setData] = useState();
+  let predefined = ["containers", "jugs", "bottles", "electronics", "propane tanks"];
+
+  let fetchResults = (URL) => {
+    fetch(URL, {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then((json) => {
+      setData(json);
+      setInit(true);
+    })
+    .catch((e) => console.log(e));
+  }
+
+  let url = `https://data.edmonton.ca/resource/gtej-pcij.json?$where=material_title like '%25${detectedObject}%25'`;
+  if (predefined.includes(detectedObject)) {
+    // go straight to question/answer flow
+    return (<QuestionModal detectedObject={detectedObject} parentCallback={parentCallback}/>)
+  } else {
+    if (!init) {
+      fetchResults(url);
+    }
+  }
+
+  if (data && data.length === 1) {
+    // go to next... i dont think this works
+    history.push('/final');
+  }
+
+  return (
+    <div>
+      {data && (data.length > 1) 
+        && data.map((data, i) => {
+          return (
+            <Link 
+                to={{
+                    pathname: "/final",
+                    state: { title: data["material_title"], result: data["stream_title"], info: data["special_instructions"]}
+                  }}
+            >
+              <Button>
+                {data["material_title"]}
+              </Button>
+            </Link>
+          )
+      })}
+    </div>
+  )
+}
+
 // the styling for questions can go here for now?
 function QuestionModal(props) {
   let {detectedObject, parentCallback} = props;
   let [result, setResult] = useState(); // "R" or "W"
-  let [materialModal, setMaterialModal] = useState(false);
+  let [materialStream, setMaterialStream] = useState();
 
   // assuming these are the only options we will handle:
   // container, jug, bottle, electronic, propane tanks
@@ -133,10 +189,13 @@ function QuestionModal(props) {
     "Is it less than 5 liters?"
   ]
 
-  // button to go to next step
-  let finishButton = (
-    <Button variant="contained" onClick={() => setMaterialModal(true)}>Finish</Button>
-  )
+  if (materialStream) {
+    return (
+      <div>
+        We recommend: {materialStream}
+      </div>
+    )
+  }
 
   //default
   let content = (<div>Next results</div>)
@@ -148,7 +207,6 @@ function QuestionModal(props) {
         {/* when buttons are clicked, should go to next page */}
         <Button variant="contained">Yes</Button>
         <Button variant="contained">No</Button>
-        {finishButton}
       </div>
     )
   }
@@ -159,7 +217,6 @@ function QuestionModal(props) {
         {jugContent[0]}
         <Button variant="contained">Yes</Button>
         <Button variant="contained">No</Button>
-        {finishButton}
       </div>
     )
   }
@@ -168,7 +225,6 @@ function QuestionModal(props) {
     content = (
       <div>
         Look for the closest disposal center near you! (google maps?)
-        {finishButton}
       </div>
     )
   }
@@ -179,25 +235,37 @@ function QuestionModal(props) {
     <div>
       <h1>Follow Up Questions: {detectedObject}</h1>
       {content}
-      <Modal
-        open={materialModal}
-        onClose={parentCallback}
-      >
-        <div className="testModal">
-          <MaterialModal detectedObject={detectedObject}/>
-        </div>
-      </Modal>
     </div>
   )
 }
 
-// The API calls can go here for now? We can rearrange stuff later
-function MaterialModal(props) {
-  let {detectedObject} = props;
+export function FinalResult(props) {
+  // if passing through link
+  // <Link 
+  //     to={{
+  //         pathname: "/final",
+  //         state: { title: ..., result: ..., info: ...}
+  //       }}
+  // >
+  // content here
+  //</Link>
+  let {title, result, info} = props.history.location.state;
 
   return (
     <div>
-      something
+    <h1>
+      {title}
+    </h1>
+    We suggest:
+    <p>
+      {result}
+    </p>
+    <p>
+      {info}
+    </p>
+    <Link to="/home">
+      Start Over
+    </Link>
     </div>
   )
 }
