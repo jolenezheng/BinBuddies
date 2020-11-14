9296560ad0278e0d484f676e22f87056b559a6d5
jolenezheng <jolenezheng18@gmail.com>
2020-11-14 17:59:52 -0500

    served react app

---
+++ server/routes/testAPI.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {   
+    quickstart();
+    res.send('API is working properly');
+});
+
+async function quickstart() {
+    // Imports the Google Cloud client libraries
+    const vision = require('@google-cloud/vision');
+    const fs = require('fs');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    const fileName = './../client/public/imgs/photo.jpg';
+
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection(fileName);
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
+
+module.exports = router;
